9db0365984e6ed29cf6fdd284a26a4095dfa0613
josefn.il@gmail.com <josefn.il@gmail.com@50215faf-7de0-7756-cd20-ed7884b09c2d>
2012-02-26 18:22:31 +0000

    cleanup

---  smid-union-find/ads-project-ui/src/mta/ads/smid/app/rich/views/View.java
+++  smid-union-find/ads-project-ui/src/mta/ads/smid/app/rich/views/View.java
@@ -1,13 +1,6 @@
 package mta.ads.smid.app.rich.views;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.Frame;
-import java.awt.Label;
-import java.awt.Rectangle;
-import java.util.Set;
-
-import javax.swing.JPanel;
 
 import mta.ads.smid.app.rich.model.IUFkForestsManager;
 import mta.ads.smid.app.ui.IUFkForestJPanel;
@@ -16,11 +9,7 @@ import mta.ads.smid.app.ui.IUFkForestJPanel.IUFkForestUnionModel;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.awt.SWT_AWT;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.IViewReference;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
-
-import edu.uci.ics.jung.visualization.picking.MultiPickedState;
 
 public class View extends ViewPart {
 
@@ -28,7 +17,7 @@ public class View extends ViewPart {
 	private final IUFkForestsManager model = IUFkForestsManager.getInstance();
 	private IUFkForestUnionModel unionModel;
 	private IUFkForestJPanel panel;
-	private int k=5;
+	private int k=2;
 	
 	@Override
 	public void dispose() {
@@ -38,27 +27,11 @@ public class View extends ViewPart {
 		}
 	}
 	
-	private MultiPickedState<Integer> getMultiPickedState(){
-    	/*
-		IViewReference[] views = PlatformUI.getWorkbench().getActiveWorkbenchWindow().
-    	getActivePage().getViewReferences();
-    	
-    	for (IViewReference ref :views){
-    		if (ref.getId().equals(View.ID)){
-    			View view = (View) ref.getView(true);
-    			return view.panel.getMultiPickedState();
-    		}
-    	}
-    	return new MultiPickedState<Integer>();
-    	*/
-		return model.getPickedState();
-	}
-	
 	public void createPartControl(Composite parent) {
 		
 		Composite swtAwtComponent = new Composite(parent, SWT.EMBEDDED | SWT.LEFT_TO_RIGHT);
 		Frame frame = SWT_AWT.new_Frame( swtAwtComponent );
-		panel = new IUFkForestJPanel(getMultiPickedState(),k,model.getN());
+		panel = new IUFkForestJPanel(model.getPickedState(),k,model.getN());
 
 		frame.add(panel);
 
@@ -70,9 +43,6 @@ public class View extends ViewPart {
 	public void setFocus() {
 	}
 	
-	public Set<Integer> getPicked(){
-		return panel.getPiked();
-	}
 	
 	public void initializeForest(){
 		panel.initializeForest(k,model.getN());
