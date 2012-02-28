907d945975666e352e5e6fb16ed34a3d826a66ff
josefn.il@gmail.com <josefn.il@gmail.com@50215faf-7de0-7756-cd20-ed7884b09c2d>
2012-02-28 08:44:32 +0000

    make comparable

---  smid-union-find/ads-project/src/mta/ads/smid/app/util/UnionPair.java
+++  smid-union-find/ads-project/src/mta/ads/smid/app/util/UnionPair.java
@@ -1,6 +1,6 @@
 package mta.ads.smid.app.util;
 
-public class UnionPair{
+public class UnionPair implements Comparable<UnionPair>{
 	public final int x;
 	public final int y;
 	public UnionPair(int x, int y) {
@@ -12,5 +12,30 @@ public class UnionPair{
 		StringBuilder builder=new StringBuilder();
 		builder.append(x).append(",").append(y);
 		return builder.toString();
+	}
+	@Override
+	public int compareTo(UnionPair unionPair) {
+		return createOrdered().toString().compareTo(unionPair.createOrdered().toString());
+	}
+	
+	
+	
+	private UnionPair createOrdered(){
+		return new UnionPair(x<y?x:y, y>x?y:x);
+	}
+	
+	@Override
+	public int hashCode() {
+		return createOrdered().toString().hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		try {
+			UnionPair unionPair=(UnionPair)obj;
+			return this.compareTo(unionPair)==0;
+		} catch (ClassCastException e){
+			return super.equals(obj);
+		}
 	}
 }
