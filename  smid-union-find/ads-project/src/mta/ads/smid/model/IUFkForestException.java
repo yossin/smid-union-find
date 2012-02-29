b2e359c025f06f185a2ce03c17066f5c97648dc4
josefn.il@gmail.com <josefn.il@gmail.com@50215faf-7de0-7756-cd20-ed7884b09c2d>
2012-02-29 08:14:52 +0000

    doc

---  smid-union-find/ads-project/src/mta/ads/smid/model/IUFkForestException.java
+++  smid-union-find/ads-project/src/mta/ads/smid/model/IUFkForestException.java
@@ -1,14 +1,31 @@
 package mta.ads.smid.model;
 
+/**
+ * Base IUFkForest exception class
+ * @author Yossi Naor & Yosi Zilberberg
+ *
+ */
 public abstract class IUFkForestException extends Exception{
 	private static final long serialVersionUID = -1647158491649459021L;
 
-	IUFkForestException(String error){
-		super(error);
+	/**
+	 * @param message error message
+	 */
+	IUFkForestException(String message){
+		super(message);
 	}
 	
+	/**
+	 * The Exception is thrown when a given element name exceeds the maximum element number (n) 
+	 * @author Yossi Naor & Yosi Zilberberg
+	 *
+	 */
 	public static class NameOutOfRangeException extends IUFkForestException{
 		private static final long serialVersionUID = -7155060852473255621L;
+		/**
+		 * @param name the name of the element
+		 * @param max the maximum value allowed
+		 */
 		public NameOutOfRangeException(int name, int max) {
 			super("element '"+name+"' is out of range. (should be between 0 and "+(max-1)+")");
 		}
