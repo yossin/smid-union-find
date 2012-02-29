a9fa4492a418b5aea8cc97d32f71af806560662b
josefn.il@gmail.com <josefn.il@gmail.com@50215faf-7de0-7756-cd20-ed7884b09c2d>
2012-02-29 08:10:37 +0000

    remove child list from union event

--- trunk/ smid-union-find/ads-project/src/mta/ads/smid/model/IUFkEvent.java
+++ trunk/ smid-union-find/ads-project/src/mta/ads/smid/model/IUFkEvent.java
@@ -1,10 +1,27 @@
 package mta.ads.smid.model;
 
-import java.util.List;
 
+/**
+ * an Interface for observing union events
+ * @author Yossi Naor & Yosi Zilberberg
+ *
+ */
 public interface IUFkEvent{
-	// case 1+3
-	void union(int orLeaf, int nrLeaf, int oldRoot, int newRoot, List<Integer> children);
-	// case 2
-	void union(int leaf1, int leaf2, int root, int child1, int child2);
+	/**
+	 * Union Event for cases 1+3
+	 * @param fromLeafId selected leaf id from source tree 
+	 * @param intoLeafId selected leaf id from destination tree 
+	 * @param fromRootId root id in the source tree (to be disposed) 
+	 * @param intoRootId root id in the destination tree
+	 */
+	void union(int fromLeafId, int intoLeafId, int fromRootId, int intoRootId);
+	/**
+	 * Union Event for case 2
+	 * @param leaf1Id selected leaf id from source tree 1
+	 * @param leaf2Id selected leaf id from source tree 2
+	 * @param newRootId new root id for managing both trees
+	 * @param rootChildId1 root id for tree 1 (to be a <i>NonRoot<i> element)
+	 * @param rootChildId2 root id for tree 2 (to be a <i>NonRoot<i> element)
+	 */
+	void union(int leaf1Id, int leaf2Id, int newRootId, int rootChildId1, int rootChildId2);
 }
