03359c25eecb86175a9e918646a7e96294ed187f
josefn.il@gmail.com <josefn.il@gmail.com@50215faf-7de0-7756-cd20-ed7884b09c2d>
2012-03-02 04:29:34 +0000

    doc

--- trunk/ smid-union-find/ads-project-ui/src/mta/ads/smid/app/rich/views/View.java
+++ trunk/ smid-union-find/ads-project-ui/src/mta/ads/smid/app/rich/views/View.java
@@ -11,46 +11,91 @@ import org.eclipse.swt.awt.SWT_AWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
+/**
+ * view for displaying IUF-k Forests
+ * 
+ * @author Yossi Naor & Yosi Zilberberg
+ *
+ */
 public class View extends ViewPart {
 
+	/**
+	 * view id
+	 */
 	public static final String ID = "ads-project-ui.view";
-	private final IUFkForestsManager model = IUFkForestsManager.getInstance();
+	/**
+     * forest manager, to delegate action
+	 */
+	private final IUFkForestsManager manager = IUFkForestsManager.getInstance();
+	/**
+	 * union model for handling events
+	 */
 	private IUFkForestUnionModel unionModel;
+	/**
+	 * forest panel for displaying - forest
+	 */
 	private IUFkForestJPanel panel;
+	/**
+	 * k-tree size 
+	 */
 	private int k=2;
 	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.part.WorkbenchPart#dispose()
+	 */
 	@Override
 	public void dispose() {
 		super.dispose();
 		if (unionModel != null){
-			model.removeObserver(unionModel);
+			manager.removeObserver(unionModel);
 		}
 	}
 	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
+	 */
 	public void createPartControl(Composite parent) {
 		Composite swtAwtComponent = new Composite(parent, SWT.EMBEDDED | SWT.LEFT_TO_RIGHT);
 		Frame frame = SWT_AWT.new_Frame( swtAwtComponent );
-		panel = new IUFkForestJPanel(model.getPickedState(),k,model.getN());
+		panel = new IUFkForestJPanel(manager.getPickedState(),k,manager.getN());
 
 		frame.add(panel);
 
 		unionModel = panel.getUnionModel();
-		model.addObserver(unionModel);
+		manager.addObserver(unionModel);
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.part.WorkbenchPart#setFocus()
+	 */
 	public void setFocus() {
 	}
 	
 	
+	/**
+	 * initialize the forest using:
+	 * <ul>
+	 * <li>k-tree size - set as a member of this class (by <i>OpenViewAction</i>)</li>
+	 * </li>n elements - from manager</li>
+	 * </ul>
+	 */
 	public void initializeForest(){
-		panel.initializeForest(k,model.getN());
+		panel.initializeForest(k,manager.getN());
 	}
 
+	/**
+	 * set k-tree size.
+	 * <br>after setting k the following happens:
+	 * <ul>
+	 * <li>set this part name (title) with the new value of k</li> 
+	 * <li>initialize forest with the new value of k</li> 
+	 * </ul>
+	 * @param k
+	 */
 	public void setK(int k) {
 		this.k=k;
 		initializeForest();
 		setPartName("IUF-"+k+" Forest");
 	}
-
 	
 }
