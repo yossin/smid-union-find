e5b992a9651e65b9db02b17815e41630259225d6
josefn.il@gmail.com <josefn.il@gmail.com@50215faf-7de0-7756-cd20-ed7884b09c2d>
2012-03-02 05:26:35 +0000

    doc

--- trunk/ smid-union-find/ads-project/app/mta/ads/smid/app/interactive/SyntaxEngine.java
+++ trunk/ smid-union-find/ads-project/app/mta/ads/smid/app/interactive/SyntaxEngine.java
@@ -6,11 +6,37 @@ import org.apache.commons.jexl2.JexlEngine;
 import org.apache.commons.jexl2.JexlException;
 import org.apache.commons.jexl2.MapContext;
 
+/**
+ * Syntax engine for parsing CLI expressions.
+ * <br>every expression will be interpreted into java code evaluation invoking handler methods
+ * @author Yossi Naor & Yosi Zilberberg
+ *
+ */
+/**
+ * @author Yossi Naor & Yosi Zilberberg
+ *
+ */
 class SyntaxEngine{
+	/**
+	 * holds the key context for the handler (at the expression map of jexl2)
+	 */
 	final static String HANDLER_PREFIX="iuf";
+	/**
+	 * engine
+	 */
 	final JexlEngine engine;
+	/**
+	 * context map
+	 */
 	final JexlContext context;
+	/**
+	 * handler
+	 */
 	final Handler handler;
+	/**
+	 * initializes: handler, engine. set the handler into the jexl2 context map
+	 * @param ui
+	 */
 	SyntaxEngine(UI ui){
 		handler = new Handler(ui);
 		engine = new JexlEngine();
@@ -19,6 +45,10 @@ class SyntaxEngine{
 		context.set(HANDLER_PREFIX, handler );
 	}
 	
+	/**
+	 * evaluate command
+	 * @param command
+	 */
 	void evaluate(String command){
 		try{
 			Expression expression = engine.createExpression(SyntaxEngine.HANDLER_PREFIX+"."+command);
--- trunk/ smid-union-find/ads-project/app/mta/ads/smid/app/interactive/UI.java
+++ trunk/ smid-union-find/ads-project/app/mta/ads/smid/app/interactive/UI.java
@@ -4,19 +4,44 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * UI
+ * @author Yossi Naor & Yosi Zilberberg
+ *
+ */
 class UI {
+	/**
+	 * buffered reader that points into System.in
+	 */
 	final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	/**
+	 * read line from buffer
+	 * @return read line
+	 * @throws IOException
+	 */
 	String readCommand() throws IOException{
 		return reader.readLine();
 	}
+	/**
+	 * print a message
+	 * @param message
+	 */
 	void message(String message){
 		System.out.println(message);
 		System.out.flush();
 	}
+	/**
+	 * print an error message
+	 * @param message
+	 */
 	void error(String message){
 		System.err.println(message);
 		System.err.flush();
 	}
+	/**
+	 * print an exception
+	 * @param e
+	 */
 	void error(Exception e){
 		System.err.println(e.getMessage());
 		System.err.flush();
