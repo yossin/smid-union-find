e1817c4a8e14fccbcfa836a83a1e6c6c6e67b994
josefn.il@gmail.com <josefn.il@gmail.com@50215faf-7de0-7756-cd20-ed7884b09c2d>
2012-03-16 13:38:55 +0000

    create a random union list

---  smid-union-find/ads-project-ui/src/mta/ads/smid/app/rich/views/UnionHistoryView.java
+++  smid-union-find/ads-project-ui/src/mta/ads/smid/app/rich/views/UnionHistoryView.java
@@ -4,13 +4,12 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.Vector;
 
 import mta.ads.smid.app.rich.model.IUFkForestsManager;
 import mta.ads.smid.app.util.UnionPair;
+import mta.ads.smid.app.util.UnionSequenceGenerator;
 
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
@@ -40,7 +39,7 @@ public class UnionHistoryView extends ViewPart {
     /**
      * forest manager, to delegate action
      */
-    private final IUFkForestsManager manager = IUFkForestsManager.getInstance();
+    private final static IUFkForestsManager manager = IUFkForestsManager.getInstance();
     /**
      * UI list viewer
      */
@@ -48,82 +47,17 @@ public class UnionHistoryView extends ViewPart {
 	/**
 	 * union list, to displayed in the list viewer
 	 */
-	final Vector<UnionPair> unionList = createUnions1();
-
+	final Vector<UnionPair> unionList = createRandomUnionList();
 	/**
-	 * create union list example
-	 * <br>eager union for making the shortest tree, in 2-trees. 
-	 * @param k k-tree size
-	 * @param n number of elements
+	 * create a random union list example
 	 * @return union sequence
 	 */
-	private static Vector<UnionPair> createUnions(int k, int n) {
-		Vector<UnionPair> unions = new Vector<UnionPair>();
-		k=k-1;
-		Queue<Integer> q1 = new LinkedList<Integer>();
-		for (int i=0;i<n;i++){
-			q1.add(i);
-		}
-		
-		boolean decrease=true;
-		do {
-			Queue<Integer> q2 = new LinkedList<Integer>();
-			
-			while (q1.isEmpty() == false){
-				int x=q1.poll();
-				q2.add(x);
-				int size=(k<q1.size()?k:q1.size());
-				for (int i=0;i<size;i++){
-					int y=q1.poll();
-					unions.add(new UnionPair(x, y));
-				}
-				if (decrease){
-					k=2;
-					decrease=false;
-				}
-			}
-			q1=q2;
-		} while(q1.size()>1);
-
-		return unions;
+	private static Vector<UnionPair> createRandomUnionList() {
+		IUFkForestsManager manager = IUFkForestsManager.getInstance();
+		List<UnionPair> list = UnionSequenceGenerator.generateRandomList(manager.getN());
+		return new Vector<UnionPair>(list);
 	}
 
-	/**
-	 * @return
-	 */
-	private static Vector<UnionPair> createUnions(){
-		Vector<UnionPair> list = new Vector<UnionPair>();
-		for (int i=0;i<10; i+=2){
-			UnionPair pair = new UnionPair(i, i+1);
-			list.add(pair);
-		}
-		return list;
-	}
-	
-	/**
-	 * @return union list example
-	 */
-	private static Vector<UnionPair> createUnions1(){
-		Vector<UnionPair> list = new Vector<UnionPair>();
-		list.add(new UnionPair(0, 1));
-		list.add(new UnionPair(0, 2));
-		list.add(new UnionPair(3, 4));
-		list.add(new UnionPair(3, 5));
-		list.add(new UnionPair(6, 7));
-		list.add(new UnionPair(6, 8));
-		list.add(new UnionPair(9, 10));
-		list.add(new UnionPair(9, 11));
-		list.add(new UnionPair(12, 13));
-		list.add(new UnionPair(12, 14));
-		list.add(new UnionPair(15, 16));
-		list.add(new UnionPair(15, 17));
-		list.add(new UnionPair(0, 3));
-		list.add(new UnionPair(6, 9));
-		list.add(new UnionPair(12, 15));
-		list.add(new UnionPair(0, 6));
-		//list.add(new UnionPair(, ));
-		return list;
-	}
 	
 	/**
 	 * dynamic proxy that will bw used for refreshing the union history list
